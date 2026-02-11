59ad9a58e0220dd27c6493712c0b3b10184cda83
Damianmusico <116502783+Damianmusico@users.noreply.github.com>
2026-02-11 19:48:10 +0100

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -3,7 +3,7 @@ import * as Component from "./quartz/components"
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
-  head: Component.Head(),
+  head: [],
   header: [],
   afterBody: [],
   footer: Component.Footer(),
