57b9c7647aebbfd00f60ed2ddebb17d03d23da33
Damianmusico <116502783+Damianmusico@users.noreply.github.com>
2026-02-11 19:01:12 +0100

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,9 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "D Acero",
-    
+    layout: {
+    left: [],
+    },
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
